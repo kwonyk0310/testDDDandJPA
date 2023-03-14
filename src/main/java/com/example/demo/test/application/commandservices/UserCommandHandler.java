df13a208566444d5d6ef8763bfa3d664888cd256
FIC08536 <ykkwon@ezens.net>
2023-03-14 10:12:00 +0900

    복합키 추가, But 시퀀스는 복합키로 사용하면 null값이 들어가게 되어 사용할 수 없다.

--- src/main/java/com/example/demo/test/application/commandservices/UserCommandHandler.java
+++ src/main/java/com/example/demo/test/application/commandservices/UserCommandHandler.java
@@ -1,6 +1,7 @@
 package com.example.demo.test.application.commandservices;
 
 import com.example.demo.test.domain.model.aggregates.User;
+import com.example.demo.test.domain.model.aggregates.UserId;
 import com.example.demo.test.domain.repository.UserRepository;
 import com.example.demo.test.interfaces.user.dto.UserSaveRqstDto;
 import lombok.RequiredArgsConstructor;
@@ -22,9 +23,13 @@ public class UserCommandHandler {
 
   public User save(UserSaveRqstDto rqstDto) {
 
+    UserId userId = UserId.builder()
+            .userId(rqstDto.getUserId())
+            .build();
+
     User userInfo = User.builder()
             .age(rqstDto.getAge())
-            .userId(rqstDto.getUserId())
+            .id(userId)
             .gender(rqstDto.getGender())
             .userName(rqstDto.getUserName())
             .build();
